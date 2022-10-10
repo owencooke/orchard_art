cb7b6e553f901fdced200426d4d4dcd7afdb40b9
owencooke <ocooke@ualberta.ca>
2022-10-10 01:10:09 -0600

    Apply JS to both slideshows

--- test.js
+++ test.js
@@ -1,27 +1,36 @@
+class Slideshow {
+    slideIndex
+
+    constructor(container) {
+        this.container = document.getElementById(container);
+    }
+
+    plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    
+    currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+    
+    showSlides(n) {
+        slides = container.getElementsByClassName("slides");
+        thumbs = container.getElementsByClassName("demo");
+        if (n > slides.length) slideIndex = 1
+        if (n < 1) slideIndex = slides.length
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < thumbs.length; i++) {
+            thumbs[i].className = thumbs[i].className.replace("active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        thumbs[slideIndex-1].className += "active";
+    }
+}
+
 window.onload = function onLoad() {
     slideIndex = 1;
-    showSlides(slideIndex);
-}
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    slides = document.getElementsByClassName("slides");
-    thumbs = document.getElementsByClassName("demo");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < thumbs.length; i++) {
-        thumbs[i].className = thumbs[i].className.replace("active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    thumbs[slideIndex-1].className += "active";
+    portaits = new Slideshow("portraits");
+    portraits.showSlides(slideIndex);
 }
